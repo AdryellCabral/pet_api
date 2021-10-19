b1420e7c47cf12e63b3476b3282c2fa49bbfc1c6
faumui <ioane.faumui@queroquitar.com.br>
2021-10-19 16:30:31 -0300

    feat: add filter pet by city

--- app/routes/pet_bp.py
+++ app/routes/pet_bp.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from app.controllers.pet_controller import post_pet, get_all, delete_data, patch_data
+from app.controllers.pet_controller import post_pet, get_all, delete_data, patch_data, filter_by_city
 
 
 bp = Blueprint("pet", __name__, url_prefix="/pet")
@@ -8,3 +8,4 @@ bp.post("")(post_pet)
 bp.get("")(get_all)
 bp.delete("")(delete_data)
 bp.patch('')(patch_data)
+bp.get('/localization')(filter_by_city)
